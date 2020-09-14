853a770b31826ee06e094d93d84e1e7fe328c3b3
tanghulu-w <946701451@qq.com>
2020-09-14 22:59:32 +0800

    完成了首页的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,27 @@
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，都会配置ajaxPrefilter对象
 $.ajaxPrefilter(function (options) {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = "http://ajax.frontend.itheima.net" + options.url;
-  });
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  options.url = "http://ajax.frontend.itheima.net" + options.url;
+
+  // 为需要请求头的ajax请求同意配置headers请求头
+  if (options.url.includes("/my/")) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+
+  options.complete = function (res) {
+    console.log("执行了回调函数");
+    console.log(res);
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      // 1. 强制清空 token
+      localStorage.removeItem("token");
+      // 2. 强制跳转到登录页面
+      location.href = "login.html";
+    }
+    renderAvatar(res.responseJSON.data);
+  };
+});
---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+$(function () {
+  getUserInfo();
+
+  // 点击实现退出功能
+  $(".tuichu").on("click", function () {
+    layer.confirm("确定退出登录?", { icon: 3, title: "提示" }, function (
+      index
+    ) {
+      //1.清空本地存储里的token值
+      localStorage.removeItem("token");
+      // 2.退出当前页面，跳转到登录页面
+      location.href = "login.html";
+      // 关闭提示框
+      layer.close(index);
+    });
+  });
+});
+
+// 获取用户的基本信息
+function getUserInfo() {
+  $.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    // headers 请求头配置对象
+    // headers: {
+    //   Authorization: localStorage.getItem("token") || "",
+    // },
+    success: function (res) {
+      console.log(res);
+      renderAvatar(res.data);
+    },
+    // 不管请求失败还是成功都会执行的回调函数
+    // complete: function (res) {
+    //   console.log("执行了回调函数");
+    //   console.log(res);
+    //   if (
+    //     res.responseJSON.status === 1 &&
+    //     res.responseJSON.message === "身份认证失败！"
+    //   ) {
+    //     // 1. 强制清空 token
+    //     localStorage.removeItem("token");
+    //     // 2. 强制跳转到登录页面
+    //     location.href = "login.html";
+    //   }
+    //   renderAvatar(res.responseJSON.data);
+    // },
+  });
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+  // 渲染用户名，用户的昵称优先渲染
+  var username = user.nickname || user.username;
+  $(".welcome").html("欢迎&nbsp;&nbsp;&nbsp;" + username);
+  // 渲染用户的头像
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").show().prop("src", user.user_pic);
+    $(".text_avatar").hide();
+  } else {
+    var text = username[0].toUpperCase();
+    $(".layui-nav-img").hide();
+    $(".text_avatar").show().html(text);
+  }
+}
